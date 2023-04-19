6c7f1664b5d0cb78665f1f1c6159b612b2defcaf
Tursunboy <tursunboyrahimjonov1@gmail.com>
2023-04-19 12:38:28 +0500

    Homework_OOP

---
+++ Modul_2_Inheritence/Modul_2_Inheritence/Group.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_2_Inheritence
+{
+    internal class Group
+    {
+        private static int MaxStudentnumber = 24;
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public int Room { get; set; }
+        public int Floor { get; set; }
+        public int[] Students = new Students[MaxStudentnumber];
+
+        public Group(int id, string name, int room, int floor, int[] student) 
+        {
+            Id = id;
+            Name = name;
+            Room = room;
+            Floor = floor;
+            Students = student;           
+        }
+       
+        public void Display()
+        {
+            Console.WriteLine($"ID - {Id}, Name - {Name}, Room - {Room}, Floor - {Floor}, Students - {Students} ");
+        }
+    }
+}
---
+++ Modul_2_Inheritence/Modul_2_Inheritence/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_2_Inheritence
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int Grade { get; set; }
+        public int Group { get; set; }
+
+    }
+}
